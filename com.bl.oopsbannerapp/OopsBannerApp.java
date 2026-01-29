3449e9744eaa8a0f59a54ca487d619f2e9aca819
BL-Purushotham <purushotham.cirimala@bridgelabz.com>
2026-01-29 15:58:00 +0530

    [Purushotham] Added: UC4: Render OOPS as Banner using String Array and Loop.

--- com.bl.oopsbannerapp/OopsBannerApp.java
+++ com.bl.oopsbannerapp/OopsBannerApp.java
@@ -1,3 +1,14 @@
+
+ /*OOPSBannerApp UC4-OOPS Banner Application (Use Case 4)
+ *
+ * This class extends the functionality of Use Case 2 by improving the way the "OOPS" banner
+ * is constructed and displayed. It focuses on enhansing code readability and efficiency
+ * by utilizing the String.join() method to create each line of the banner. This approach
+ * overcomes the drawbacks of using the + oprator for String concatination, which can lead
+ * to code inefficiency due to creation of multiple intermediate String objects in memory.
+ * This use case improves upon UC3 by using a String array to store banner lines
+ * and iterating through them with a for-each loop, eliminating hardcoded print
+ */
 public class OopsBannerApp {
     public static void main(String[] args) {
         // Line 1
